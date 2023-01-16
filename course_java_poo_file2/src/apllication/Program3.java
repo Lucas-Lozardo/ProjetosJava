a00f097c7c0e02795feb2e3e58b477adfc902007
Lucas Lozardo <lucas_losardo@hotmail.com>
2023-01-16 11:41:49 -0300

    add new program named File2

---
+++ course_java_poo_file2/src/apllication/Program3.java
@@ -0,0 +1,36 @@
+package apllication;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		
+
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("Enter a folder path: ");
+			String strPath = sc.nextLine();
+			
+			File path = new File(strPath);
+			///Localização de pastas nos diretórios
+			File[] folders = path.listFiles(File :: isDirectory);
+			System.out.println("FOLDERS: ");
+			for(File folder : folders) {
+				System.out.println(folder);
+			}
+			
+			///Localização de arquivos nos diretórios
+			File[] files = path.listFiles(File :: isFile);
+			System.out.println("FILES: ");
+			for(File folder : files) {
+				System.out.println(folder);
+			}
+			
+			///Criação de subpastas no diretório
+			boolean sucess = new File(strPath + "/subdir").mkdir();
+			sc.close();
+	}
+
+}
