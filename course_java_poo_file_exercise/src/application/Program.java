f1fab575265509aa336f5b3ac23fd226bc1ba0fb
Lucas Lozardo <lucas_losardo@hotmail.com>
2023-01-18 21:20:30 -0300

    added new comments in Program file exercise

--- course_java_poo_file_exercise/src/application/Program.java
+++ course_java_poo_file_exercise/src/application/Program.java
@@ -22,36 +22,48 @@ public class Program {
 		
 		List<Product> list = new ArrayList<>();
 		
+		///Leitura do arquivo de entrada e criaçáo da pasta onde será inserido o novo arquivo.
+		
+		///Ler o caminho de entrada do arquivo (incluindo o arquivo)
 		System.out.println("Enter file path: ");
 		String sourceFileStr = sc.nextLine();
-		
+		///Instancia o caminho do arquivo de entrada no File (incluindo o arquivo)
 		File sourceFile = new File(sourceFileStr);
-		String sourceFolderStr = sourceFile.getParent();
-		
-		boolean sucess = new File(sourceFolderStr + "/out").mkdir();
+		///Instancia numa String o caminho do arquivo de entrada desprezando o arquivo em si.
+		String sourceFolderStr = sourceFile.getParent(); 	/// .getParent() = ler o caminho do arquivo de entrada desprezando o arquivo.
+		///Criar a nova pasta onde será inserido o arquivo posteriormente
+		boolean sucess = new File(sourceFolderStr + "/out").mkdir(); /// .mkdir() = Comando para criar um novo diretóri (a pasta). + "/out" = nome da pasta que será criada.
 		System.out.println("Directory created succesfully: " + sucess);
 		
+		///Criação do novo arquivo que será criado dentro da pasta
 		String targetFilerStr = sourceFolderStr + "/out/summary.csv";
 		
+		///Ler o arquivo, fragmentar e inserir nas variáveis correspondentes as informações obtidas utilizando vetores. (String, Double, Int etc...).
+		
+		///Ler e armazenar numa String o arquivo do caminho de entrada.
 		try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+			///Ler a primeira linha do arquivo ao instancia-lo.
 			String itemCsv = br.readLine();
 			
+			
+			///Fragmentar e inserir utilizando vetores
 			while(itemCsv != null) {
-			String[] fields = itemCsv.split(",");	
+			String[] fields = itemCsv.split(","); ///.split(",") = fragmentar utilizando como base a vírgula inseriada no arquivo de entrada (origem).
 				String name = fields[0];
-				double price = Double.parseDouble(fields[1]);
-				int quantity = Integer.parseInt(fields[2]);
+				double price = Double.parseDouble(fields[1]); ///Double.parseDouble = transformar a String obtida em Double.
+				int quantity = Integer.parseInt(fields[2]); ///Integer.parseInt = transformar a String obtida em  Integer.
 				
 				list.add(new Product(name, price, quantity));
-				
+				///Ler a próxima linha.
 				itemCsv = br.readLine();
 			}
 			
+			///Escrever no novo arquivo que foi instanciado acima, linha 39.
 			try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFilerStr))){
 				
 				for(Product item : list) {
 					bw.write(item.getName() + ", " + String.format("%.2f", item.total()));
-					bw.newLine();
+					bw.newLine(); /// newLine() = criar nova linha.
 				}
 				System.out.println(targetFilerStr + " CREATED");
 			}
