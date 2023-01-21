fdb1a6a2ac95487383664caf83aff9e6364b52d8
Lucas Lozardo <lucas_losardo@hotmail.com>
2023-01-21 17:27:08 -0300

    update

--- course_java_poo_interface_exercise/src/model/service/PaypalService.java
+++ course_java_poo_interface_exercise/src/model/service/PaypalService.java
@@ -2,17 +2,15 @@ package model.service;
 
 public class PaypalService implements OnlinePaymentService{
 	
-	private static final double FEE_PERCENTAGE = 0.02;
-	private static final double MONTHLY_INTEREST = 0.01;
 	
 	@Override
 	public Double paymentFee(Double amount) {
-		return amount * FEE_PERCENTAGE;
+		return amount * 0.02;
 	}
 	
 	@Override
 	public Double interest(Double amount, Integer months) {
-		return amount * MONTHLY_INTEREST * months;
+		return amount * 0.01 * months;
 	}
 
 }
