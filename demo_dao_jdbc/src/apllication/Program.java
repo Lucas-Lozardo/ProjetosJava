fb763fdfadd4027a1cd2cdcb39eecf8317a3d138
Lucas Lozardo <lucas_losardo@hotmail.com>
2023-02-15 19:07:54 -0300

    add at db.properties allowPublicKeyRetrieval

--- demo_dao_jdbc/src/apllication/Program.java
+++ demo_dao_jdbc/src/apllication/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== TEST 1: Seller findByDepartment ===");
+		System.out.println("\n=== TEST 2: Seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
